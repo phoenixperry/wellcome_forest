e54e3998079a088647d7979dd2a655d9cf90d002
Mark Kvetny <m.kvetny@outlook.com>
2020-02-04 20:06:27 +0000

    add serial and bunch of states

--- Arduino/tree/tree.ino
+++ Arduino/tree/tree.ino
@@ -1,5 +1,4 @@
 #include "FastLED.h"
-#include "AsyncDelay.h"
 
 #define LED_PIN 10
 #define LED_COUNT 32
@@ -11,10 +10,10 @@
 
 CRGB leds[LED_COUNT];
 bool button_pressed = false;
-char tree_id = 'C'; // int from C - J; C is the starting tree
-uint16_t time = 0;  // timer used for animations
+String tree_id = "C"; // int from C - J; C is the starting tree
+uint16_t time = 0;    // timer used for animations
 
-enum Color
+enum State
 {
   idle,
   beacon,
@@ -24,13 +23,15 @@ enum Color
   fail
 };
 
-bool is_beacon = tree_id == 'C';
+bool is_beacon = tree_id == "C";
 
-Color current_state = is_beacon ? beacon : idle;
+State current_state = is_beacon ? beacon : idle;
 double hue = is_beacon ? 20.0 : 100.0;
 
 void setup()
 {
+  // setup Serial
+  Serial.begin(9600);
   // setup neopixel
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
   // setup button
@@ -48,10 +49,15 @@ void setHSV(double h, double s, double v)
 
 void loop()
 {
+  time++;
   /*--- IDLE STATE ---*/
   if (current_state == idle)
   {
-    setHSV(100, 255, 255);
+    hue = 100;
+    double speed = 0.002;
+    double saturation = 150 + abs(sin(time * speed)) * 50;
+    double value = 255 - abs(cos(time * speed)) * 55;
+    setHSV(hue, saturation, value);
   }
   /*--- BEACON STATE ---*/
   else if (current_state == beacon)
@@ -59,14 +65,14 @@ void loop()
     if (!button_pressed)
     {
       // beacon animation
-      time++;
-      hue = sin(time * 0.007) * 30 + 50;
-      setHSV(hue, 255, 255);
+      hue = -sin(time * 0.007) * 30 + 50;
+      double value = 220 + sin(time * 0.007) * 35;
+      setHSV(hue, 255, value);
       // handle button press
       if (digitalRead(BUTTON_IN) == LOW)
       {
+        Serial.println(tree_id + " pressed");
         button_pressed = true;
-        Serial.println();
       }
     }
     else
@@ -77,7 +83,14 @@ void loop()
     }
   }
   /*--- WAITING STATE ---*/
+  if (current_state == waiting)
+  {
+  }
   /*--- FAIL STATE ---*/
+  if (current_state == fail)
+  {
+    setHSV(hue, 240, 200);
+  }
   /*--- WIN STATE ---*/
   /*--- WIN_GLOBAL STATE ---*/
 }
